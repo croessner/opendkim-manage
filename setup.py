81f896d704023cb64721ac8e0e9ea3b1fea53bfb
Christian Roessner <c@roessner-network-solutions.com>
2018-01-25 16:15:52 +0100

    [Add] Initial import

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='opendkim-manage',
+    version='0.1',
+    packages=[''],
+    package_dir={'src'},
+    url='https://github.com/croessner/opendkim-manage',
+    license='GPL2',
+    author='Christian Roessner',
+    author_email='c@roessner.co',
+    description='OpenDKIM management tool for key rollover',
+    install_requires=['dns', 'ldap', 'M2Crypto']
+)
